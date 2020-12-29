994327d21980907093d260dd35cc5287608ae6ce
Rakesh Shrestha <rakeshsh31@gmail.com>
2020-12-28 17:18:06 -0800

    reformatting laserscan_multi_merger.cpp: tabs to spaces

--- src/laserscan_multi_merger.cpp
+++ src/laserscan_multi_merger.cpp
@@ -57,86 +57,86 @@ private:
 
 // void LaserscanMerger::reconfigureCallback(laserscan_multi_mergerConfig &config, uint32_t level)
 // {
-// 	this->angle_min = config.angle_min;
-// 	this->angle_max = config.angle_max;
-// 	this->angle_increment = config.angle_increment;
-// 	this->time_increment = config.time_increment;
-// 	this->scan_time = config.scan_time;
-// 	this->range_min = config.range_min;
-// 	this->range_max = config.range_max;
+//  this->angle_min = config.angle_min;
+//  this->angle_max = config.angle_max;
+//  this->angle_increment = config.angle_increment;
+//  this->time_increment = config.time_increment;
+//  this->scan_time = config.scan_time;
+//  this->range_min = config.range_min;
+//  this->range_max = config.range_max;
 // }
 
 void LaserscanMerger::laserscan_topic_parser()
 {
   // TODO: get all topics
-	// LaserScan topics to subscribe
-	// rclcpp::master::V_TopicInfo topics;
-	// rclcpp::master::getTopics(topics);
+  // LaserScan topics to subscribe
+  // rclcpp::master::V_TopicInfo topics;
+  // rclcpp::master::getTopics(topics);
 
     istringstream iss(laserscan_topics);
-	vector<string> tokens;
-	copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter<vector<string> >(tokens));
-	vector<string> tmp_input_topics;
+  vector<string> tokens;
+  copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter<vector<string> >(tokens));
+  vector<string> tmp_input_topics;
 
   // TODO: filter topics
   tmp_input_topics = tokens;
 
-	// for(int i=0;i<tokens.size();++i)
-	// {
+  // for(int i=0;i<tokens.size();++i)
+  // {
   //   for(int j=0;j<topics.size();++j)
-	// 	{
-	// 		if( (tokens[i].compare(topics[j].name) == 0) && (topics[j].datatype.compare("sensor_msgs/msg/LaserScan") == 0) )
-	// 		{
-	// 			tmp_input_topics.push_back(topics[j].name);
-	// 		}
-	// 	}
-	// }
+  //  {
+  //    if( (tokens[i].compare(topics[j].name) == 0) && (topics[j].datatype.compare("sensor_msgs/msg/LaserScan") == 0) )
+  //    {
+  //      tmp_input_topics.push_back(topics[j].name);
+  //    }
+  //  }
+  // }
 
-	sort(tmp_input_topics.begin(),tmp_input_topics.end());
-	std::vector<string>::iterator last = std::unique(tmp_input_topics.begin(), tmp_input_topics.end());
-	tmp_input_topics.erase(last, tmp_input_topics.end());
+  sort(tmp_input_topics.begin(),tmp_input_topics.end());
+  std::vector<string>::iterator last = std::unique(tmp_input_topics.begin(), tmp_input_topics.end());
+  tmp_input_topics.erase(last, tmp_input_topics.end());
 
 
-	// Do not re-subscribe if the topics are the same
-	if( (tmp_input_topics.size() != input_topics.size()) || !equal(tmp_input_topics.begin(),tmp_input_topics.end(),input_topics.begin()))
-	{
+  // Do not re-subscribe if the topics are the same
+  if( (tmp_input_topics.size() != input_topics.size()) || !equal(tmp_input_topics.begin(),tmp_input_topics.end(),input_topics.begin()))
+  {
 
     // Hopefully the subscribers use RAII and shutdown after reallocation
     // TODO: check if reallocation will shutdown older subscribers
-		// Unsubscribe from previous topics
-		// for(int i=0; i<scan_subscribers.size(); ++i)
-		// 	scan_subscribers[i].shutdown();
+    // Unsubscribe from previous topics
+    // for(int i=0; i<scan_subscribers.size(); ++i)
+    //  scan_subscribers[i].shutdown();
 
-		input_topics = tmp_input_topics;
-		if(input_topics.size() > 0)
-		{
-            scan_subscribers.resize(input_topics.size());
-			clouds_modified.resize(input_topics.size());
-			clouds.resize(input_topics.size());
+    input_topics = tmp_input_topics;
+    if(input_topics.size() > 0)
+    {
+      scan_subscribers.resize(input_topics.size());
+      clouds_modified.resize(input_topics.size());
+      clouds.resize(input_topics.size());
       RCLCPP_INFO(this->get_logger(),"Subscribing to topics\t%ld",scan_subscribers.size());
-			for(int i=0; i<input_topics.size(); ++i)
-			{
+      for(int i=0; i<input_topics.size(); ++i)
+      {
         scan_subscribers[i] = this->create_subscription<sensor_msgs::msg::LaserScan>(
             input_topics[i].c_str(), 1,
             // need explicit casting from std::Bind to specific typed std::function
             (std::function<void(const sensor_msgs::msg::LaserScan::ConstSharedPtr msg)>)
               std::bind(&LaserscanMerger::scanCallback, this, std::placeholders::_1, input_topics[i])
         );
-				clouds_modified[i] = false;
-				cout << input_topics[i] << " ";
-			}
-		}
-		else
+        clouds_modified[i] = false;
+        cout << input_topics[i] << " ";
+      }
+    }
+    else
     {
       RCLCPP_INFO(this->get_logger(),"Not subscribed to any topic.");
     }
-	}
+  }
 }
 
 LaserscanMerger::LaserscanMerger()
   : Node("laser_multi_merger")
 {
-	// rclcpp::Node nh("~");
+  // rclcpp::Node nh("~");
 
   this->declare_parameter<std::string>("destination_frame", "cart_frame");
   this->declare_parameter<std::string>("cloud_destination_topic", "/merged_cloud");
@@ -161,12 +161,12 @@ LaserscanMerger::LaserscanMerger()
   this->get_parameter("range_min", range_min);
   this->get_parameter("range_max", range_max);
 
-    this->laserscan_topic_parser();
+  this->laserscan_topic_parser();
 
-	// point_cloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2> (cloud_destination_topic.c_str(), 1, false);
-	// laser_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan> (scan_destination_topic.c_str(), 1, false);
-	point_cloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2> (cloud_destination_topic.c_str(), 1);
-	laser_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan> (scan_destination_topic.c_str(), 1);
+  // point_cloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2> (cloud_destination_topic.c_str(), 1, false);
+  // laser_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan> (scan_destination_topic.c_str(), 1, false);
+  point_cloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2> (cloud_destination_topic.c_str(), 1);
+  laser_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan> (scan_destination_topic.c_str(), 1);
 
   tfBuffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
   tfListener_ = std::make_shared<tf2_ros::TransformListener>(*tfBuffer_);
@@ -175,19 +175,19 @@ LaserscanMerger::LaserscanMerger()
 void LaserscanMerger::scanCallback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan, std::string topic)
 {
   sensor_msgs::msg::PointCloud2 tmpCloud1,tmpCloud2;
-	// sensor_msgs::msg::PointCloud2 tmpCloud3;
+  // sensor_msgs::msg::PointCloud2 tmpCloud3;
 
-    // Verify that TF knows how to transform from the received scan to the destination scan frame
+  // Verify that TF knows how to transform from the received scan to the destination scan frame
   // No need to wait for transform with tf2
-	// tfListener_.waitForTransform(scan->header.frame_id.c_str(), destination_frame.c_str(), scan->header.stamp, rclcpp::Duration(1));
+  // tfListener_.waitForTransform(scan->header.frame_id.c_str(), destination_frame.c_str(), scan->header.stamp, rclcpp::Duration(1));
 
   projector_.transformLaserScanToPointCloud(scan->header.frame_id, *scan, tmpCloud1, *tfBuffer_, laser_geometry::channel_option::Distance);
 
   geometry_msgs::msg::TransformStamped transform;
-	try
-	{
-		transform = tfBuffer_->lookupTransform(destination_frame, tmpCloud1.header.frame_id, rclcpp::Time(0));
-	}
+  try
+  {
+    transform = tfBuffer_->lookupTransform(destination_frame, tmpCloud1.header.frame_id, rclcpp::Time(0));
+  }
   catch (tf2::TransformException &ex)
   {
     RCLCPP_ERROR(this->get_logger(), "%s",ex.what());
@@ -196,99 +196,99 @@ void LaserscanMerger::scanCallback(const sensor_msgs::msg::LaserScan::ConstShare
 
   tf2::doTransform(tmpCloud1, tmpCloud2, transform);
 
-	for(int i=0; i<input_topics.size(); ++i)
-	{
-		if(topic.compare(input_topics[i]) == 0)
-		{
+  for(int i=0; i<input_topics.size(); ++i)
+  {
+    if(topic.compare(input_topics[i]) == 0)
+    {
       // sensor_msgs::convertPointCloudToPointCloud2(tmpCloud2,tmpCloud3);
-			pcl_conversions::toPCL(tmpCloud2, clouds[i]);
-			clouds_modified[i] = true;
-		}
-	}	
+      pcl_conversions::toPCL(tmpCloud2, clouds[i]);
+      clouds_modified[i] = true;
+    }
+  }
 
     // Count how many scans we have
-	int totalClouds = 0;
-	for(int i=0; i<clouds_modified.size(); ++i)
-		if(clouds_modified[i])
-			++totalClouds;
+  int totalClouds = 0;
+  for(int i=0; i<clouds_modified.size(); ++i)
+    if(clouds_modified[i])
+      ++totalClouds;
 
     // Go ahead only if all subscribed scans have arrived
-	if(totalClouds == clouds_modified.size())
-	{
-		pcl::PCLPointCloud2 merged_cloud = clouds[0];
-		clouds_modified[0] = false;
+  if(totalClouds == clouds_modified.size())
+  {
+    pcl::PCLPointCloud2 merged_cloud = clouds[0];
+    clouds_modified[0] = false;
 
-		for(int i=1; i<clouds_modified.size(); ++i)
-		{
-			pcl::concatenate(merged_cloud, clouds[i], merged_cloud);
-			clouds_modified[i] = false;
-		}
-	
+    for(int i=1; i<clouds_modified.size(); ++i)
+    {
+      pcl::concatenate(merged_cloud, clouds[i], merged_cloud);
+      clouds_modified[i] = false;
+    }
+
     // TODO: make the point cloud topic publishable as well
-		// point_cloud_publisher_->publish(merged_cloud);
+    // point_cloud_publisher_->publish(merged_cloud);
 
-		Eigen::MatrixXf points;
-		getPointCloudAsEigen(merged_cloud,points);
+    Eigen::MatrixXf points;
+    getPointCloudAsEigen(merged_cloud,points);
 
-		pointcloud_to_laserscan(points, &merged_cloud);
-	}
+    pointcloud_to_laserscan(points, &merged_cloud);
+  }
 }
 
 void LaserscanMerger::pointcloud_to_laserscan(Eigen::MatrixXf points, pcl::PCLPointCloud2 *merged_cloud)
 {
   sensor_msgs::msg::LaserScan::SharedPtr output = std::make_shared<sensor_msgs::msg::LaserScan>();
-	output->header = pcl_conversions::fromPCL(merged_cloud->header);
-	output->header.frame_id = destination_frame.c_str();
+  output->header = pcl_conversions::fromPCL(merged_cloud->header);
+  output->header.frame_id = destination_frame.c_str();
   output->header.stamp = this->now();  //fixes #265
-	output->angle_min = this->angle_min;
-	output->angle_max = this->angle_max;
-	output->angle_increment = this->angle_increment;
-	output->time_increment = this->time_increment;
-	output->scan_time = this->scan_time;
-	output->range_min = this->range_min;
-	output->range_max = this->range_max;
+  output->angle_min = this->angle_min;
+  output->angle_max = this->angle_max;
+  output->angle_increment = this->angle_increment;
+  output->time_increment = this->time_increment;
+  output->scan_time = this->scan_time;
+  output->range_min = this->range_min;
+  output->range_max = this->range_max;
 
-	uint32_t ranges_size = std::ceil((output->angle_max - output->angle_min) / output->angle_increment);
-	output->ranges.assign(ranges_size, output->range_max + 1.0);
+  uint32_t ranges_size = std::ceil((output->angle_max - output->angle_min) / output->angle_increment);
+  output->ranges.assign(ranges_size, output->range_max + 1.0);
 
-	for(int i=0; i<points.cols(); i++)
-	{
-		const float &x = points(0,i);
-		const float &y = points(1,i);
-		const float &z = points(2,i);
+  for(int i=0; i<points.cols(); i++)
+  {
+    const float &x = points(0,i);
+    const float &y = points(1,i);
+    const float &z = points(2,i);
 
-		if ( std::isnan(x) || std::isnan(y) || std::isnan(z) )
-		{
-			RCLCPP_DEBUG(this->get_logger(), "rejected for nan in point(%f, %f, %f)\n", x, y, z);
-			continue;
-		}
+    if ( std::isnan(x) || std::isnan(y) || std::isnan(z) )
+    {
+      RCLCPP_DEBUG(this->get_logger(), "rejected for nan in point(%f, %f, %f)\n", x, y, z);
+      continue;
+    }
 
-		double range_sq = y*y+x*x;
-		double range_min_sq_ = output->range_min * output->range_min;
-		if (range_sq < range_min_sq_) {
-			RCLCPP_DEBUG(this->get_logger(), "rejected for range %f below minimum value %f. Point: (%f, %f, %f)", range_sq, range_min_sq_, x, y, z);
-			continue;
-		}
+    double range_sq = y*y+x*x;
+    double range_min_sq_ = output->range_min * output->range_min;
+    if (range_sq < range_min_sq_) {
+      RCLCPP_DEBUG(this->get_logger(), "rejected for range %f below minimum value %f. Point: (%f, %f, %f)", range_sq, range_min_sq_, x, y, z);
+      continue;
+    }
 
-		double angle = atan2(y, x);
-		if (angle < output->angle_min || angle > output->angle_max)
-		{
-			RCLCPP_DEBUG(this->get_logger(), "rejected for angle %f not in range (%f, %f)\n", angle, output->angle_min, output->angle_max);
-			continue;
-		}
-		int index = (angle - output->angle_min) / output->angle_increment;
+    double angle = atan2(y, x);
+    if (angle < output->angle_min || angle > output->angle_max)
+    {
+      RCLCPP_DEBUG(this->get_logger(), "rejected for angle %f not in range (%f, %f)\n", angle, output->angle_min, output->angle_max);
+      continue;
+    }
+    int index = (angle - output->angle_min) / output->angle_increment;
 
 
-		if (output->ranges[index] * output->ranges[index] > range_sq)
-			output->ranges[index] = sqrt(range_sq);
-	}
+    if (output->ranges[index] * output->ranges[index] > range_sq)
+      output->ranges[index] = sqrt(range_sq);
+  }
 
-	laser_scan_publisher_->publish(*output);
+  laser_scan_publisher_->publish(*output);
 }
 
 int main(int argc, char** argv)
 {
-	rclcpp::init(argc, argv);
+  rclcpp::init(argc, argv);
 
   LaserscanMerger::SharedPtr _laser_merger = std::make_shared<LaserscanMerger>();
 
@@ -298,8 +298,8 @@ int main(int argc, char** argv)
     // f = std::bind(&LaserscanMerger::reconfigureCallback,&_laser_merger, _1, _2);
     // server.setCallback(f);
 
-	rclcpp::spin(_laser_merger);
+  rclcpp::spin(_laser_merger);
   rclcpp::shutdown();
 
-	return 0;
+  return 0;
 }
